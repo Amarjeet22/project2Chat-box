0ed8395648f9d0228a060c9c7d9960874709ddb3
Amar <amarjeetranjan594@gmail.com>
2023-02-18 17:13:34 +0530

    first commit

---
+++ sctipt.js
@@ -0,0 +1,4 @@
+const nots = document.querySelector('.send-btn');
+nots.addEventListener('click',function(){
+    alert("sorry you can't chat right now this project is in development process ");
+})
